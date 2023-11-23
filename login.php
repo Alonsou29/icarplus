2ddba9fb79e752ac5ebf749dfc410f05141b0a93
Alonsou29 <alonsourdaneta.29@hotmail.com>
2023-11-23 15:34:44 -0400

    segundo commit

--- login.php
+++ login.php
@@ -5,10 +5,13 @@ session_start();
 $_SESSION['usuario']= $_POST['usuario'];
 $_SESSION['pass'] = $_POST['pass'];
 
-$buscadato = mysqli_query($conn,"SELECT * FROM usuarios_icarplus WHERE Usuario = '".$_SESSION['usuario']."and Password ='".$_SESSION['pass']."'"); //aqui va la consulta
 
-if($_SESSION['usuario'] === '' || $_SESSION['pass'] === ''){
-        echo json_encode("error".$b);
+$buscadato = mysqli_query($conn,"SELECT * FROM usuarios_icarplus WHERE Usuario ='".$_SESSION['usuario']."'and Pass = '".$_SESSION['pass']."'")or die(mysqli_error($conn)); //aqui va la consulta
+$b = mysqli_num_rows($buscadato);
+
+
+if($b == 0){
+        echo json_encode("error");
 }else{echo json_encode("esoooo");}
 
 
